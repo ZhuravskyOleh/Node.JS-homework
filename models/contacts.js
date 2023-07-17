8f90fecf622c72a3f991efa2e905d231b9201957
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-07-17 13:51:14 +0300

    start files

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,71 @@
-// import fs from 'fs/promises'
+// // import fs from 'fs/promises'
 
-const listContacts = async () => {}
+// const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+// const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+// const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+// const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+// const updateContact = async (contactId, body) => {}
+
+// export default {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// }
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+const updateContactsStorage = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+
+export const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+};
+
+export const getContactById = async (id) => {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === id);
+    return result || null;
+};
+
+export const removeContact = async (id) => {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === id);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContactsStorage(contacts);
+    return result;
+};
+
+export const addContact = async ({name, email, phone}) => {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone,
+    };
+    contacts.push(newContact);
+    await updateContactsStorage(contacts);
+    return newContact;
+};
 
 export default {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
 }
