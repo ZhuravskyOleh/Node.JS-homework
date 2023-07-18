457589e6e7862fe9c9419c2b66e6b82efa4e995a
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-07-18 12:51:45 +0300

    add middlewar

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,6 +2,7 @@ import express from "express";
 import contactsController from "../../controllers/contacts-controller.js"
 import { validateBody } from "../../decorators/index.js";
 import contactsSchemas from "../../Schemas/contacts-schema.js";
+import { isEmptyBody } from "../../middlewars/index.js";
 
 const contactsRouter = express.Router()
 
@@ -9,10 +10,10 @@ contactsRouter.get('/', contactsController.getAll);
 
 contactsRouter.get('/:contactId', contactsController.getById);
 
-contactsRouter.post('/', validateBody(contactsSchemas.contactAddSchema), contactsController.addContact);
+contactsRouter.post('/', isEmptyBody, validateBody(contactsSchemas.contactAddSchema), contactsController.addContact);
 
 contactsRouter.delete('/:contactId', contactsController.deleteById);
 
-contactsRouter.put('/:contactId', validateBody(contactsSchemas.contactAddSchema), contactsController.updateById);
+contactsRouter.put('/:contactId', isEmptyBody, validateBody(contactsSchemas.contactAddSchema), contactsController.updateById);
 
 export default contactsRouter;
