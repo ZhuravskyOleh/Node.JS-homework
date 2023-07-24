5e044696032c0058340ba3f7d46f8a3f98990f08
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-07-24 15:43:06 +0300

    hw-3

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,18 +2,20 @@ import express from "express";
 import contactsController from "../../controllers/contacts-controller.js"
 import { validateBody } from "../../decorators/index.js";
 import contactsSchemas from "../../Schemas/contacts-schema.js";
-import { isEmptyBody } from "../../middlewars/index.js";
+import { isEmptyBody, isValidId } from "../../middlewars/index.js";
 
 const contactsRouter = express.Router()
 
 contactsRouter.get('/', contactsController.getAll);
 
-contactsRouter.get('/:contactId', contactsController.getById);
+contactsRouter.get('/:contactId', isValidId, contactsController.getById);
 
 contactsRouter.post('/', isEmptyBody, validateBody(contactsSchemas.contactAddSchema), contactsController.addContact);
 
-contactsRouter.delete('/:contactId', contactsController.deleteById);
+contactsRouter.delete('/:contactId', isValidId, contactsController.deleteById);
 
-contactsRouter.put('/:contactId', isEmptyBody, validateBody(contactsSchemas.contactAddSchema), contactsController.updateById);
+contactsRouter.put('/:contactId', isEmptyBody, isValidId, validateBody(contactsSchemas.contactAddSchema), contactsController.updateById);
+
+contactsRouter.patch('/:contactId/favorite', isEmptyBody, isValidId, validateBody(contactsSchemas.contactUpdateFavoriteSchema), contactsController.updateFavorite);
 
 export default contactsRouter;
--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+dotenv.config();
+
+const { DB_HOST, PORT } = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful.")
+    });
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+// wIdRI3J1I7o2CahR
+
+
